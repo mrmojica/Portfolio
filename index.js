b2813d42a0e77a6a7d1ed0fa8931514beb82366d
mrmojica <mrmojica510@gmail.com>
2017-03-17 10:06:37 -0700

    added simple nav

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+$("#nav-about").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        1000);
+});
+
+$("#nav-work").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#work").offset().top},
+        1250);
+});
+
+$("#nav-contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        1500);
+});
+
 $(".arrow-link").click(function() {
     $('html,body').animate({
         scrollTop: $("#about").offset().top},
@@ -9,3 +27,4 @@ $(".contact-link").click(function() {
         scrollTop: $("#contact").offset().top},
         2000);
 });
+
